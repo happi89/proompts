576f681b995b3abe2c2ddfc82c756b06a28cfb0f
Farhaan Patel <farhaanp99@gmail.com>
2023-06-19 01:07:16 -0400

    added loading and removed hidden style for small devices in profile layout

--- app/profile/layout.tsx
+++ app/profile/layout.tsx
@@ -15,7 +15,7 @@ const sidebarNavItems = [
 
 const ProfileLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="hidden w-full p-10 pb-16 space-y-6 md:block">
+    <div className="w-full p-10 pb-16 space-y-6">
       <div className="space-y-0.5">
         <h2 className="text-2xl font-bold tracking-tight">Account</h2>
         <p className="text-muted-foreground">
